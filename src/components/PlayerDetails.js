363af3f0b69704c4fc86f707237558a432e89b7a
chrisclarkedev <cclarke8904@gmail.com>
2022-05-13 11:42:33 -0400

    cplayer controls styling

--- src/components/PlayerDetails.js
+++ src/components/PlayerDetails.js
@@ -7,7 +7,7 @@ function PlayerDetails(props) {
         <img src={props.song.img_src} alt="" />
       </div>
       <h3 className="details-title">{props.song.title}</h3>
-      <h4 className="detail-artist">{props.song.artist}</h4>
+      <h4 className="details-artist">{props.song.artist}</h4>
     </div>
   );
 }
