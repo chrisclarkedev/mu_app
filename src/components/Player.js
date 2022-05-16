0288d7de9ee4e59e37dcb010bec582434acd180a
chrisclarkedev <cclarke8904@gmail.com>
2022-05-16 18:26:30 -0400

    passed props, set function for skipping and rewinding songs, audio source

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,23 +5,47 @@ import PlayerControls from './PlayerControls';
 function Player(props) {
   const audioElement = useRef(null);
   const [playing, setPlaying] = useState(false);
-  //Dont want songs to play as soon as app starts
+  // Dont want songs to play as soon as app starts
 
   useEffect(() => {
     if (playing) {
       audioElement.current.play();
-      //Anytime something changes it will check if audio is playing and will continue playing
+      // Anytime something changes it will check if audio is playing and will continue playing
     } else {
       audioElement.current.pause();
       // It will just pause the song
     }
   });
 
-  const skipSong = (forwards = true) => {};
+  const skipSong = (forwards = true) => {
+    // If going forward we pass true, if the opposite backwards we pass false
+    if (forwards) {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp++;
+        // Create a temp for current song and add 1 to go to next song
+        if (temp > props.songs.length - 1) {
+          temp = 0;
+        }
+
+        return temp;
+      });
+    } else {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp--;
+        // Create temp for current song and minus 1 to go to previous song
+        if (temp < 0) {
+          temp = props.songs.length - 1;
+        }
+        // Will retrieve previous song index and set it to previous song
+        return temp;
+      });
+  };
 
   return (
     <div className="c-player">
-      <audio ref={audioElement}></audio>
+      <audio src={props.songs[props.currentSongIndex].src} ref={audioElement}></audio>
       <h4>Now Playing</h4>
       <PlayerDetails song={props.song} />
       <PlayerControls />
