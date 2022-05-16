c5dc11e5952bd2e373d72c6445622c3e82875d2d
chrisclarkedev <cclarke8904@gmail.com>
2022-05-16 17:47:04 -0400

    Added function which goes through array of songs to next song or restart list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useStat, useEffect } from 'react';
+//Call function when things change (lifestyle method)
 import Player from './components/Player';
 
 function App() {
-  const [songs, setSongs] = useState([
+  const [songs] = useState([
     {
       title: 'Pop Thieves (Makie It Feel Good)',
       artist: 'Childish Gambino',
@@ -38,9 +39,28 @@ function App() {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+  // If song skips to the next song and is more than array we will start at beginning of song list
+  // If song doesnt skip to 0 then it will add 1 to the array going to next song
+  // Next song will update depending on what currentSongIndex is
+  //useEffect function that will be initiated once currentSongIndex is called
+
   return (
     <div className="App">
-      <Player song={songs[currentSongIndex]} nextSong={songs[nextSongIndex]} />
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
