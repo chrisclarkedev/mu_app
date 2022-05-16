49b348f4c35359119d355f7408f65ac69f64cb00
chrisclarkedev <cclarke8904@gmail.com>
2022-05-16 18:08:53 -0400

    imported state, use effect functionality

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PlayerDetails from './PlayerDetails';
 import PlayerControls from './PlayerControls';
 
 function Player(props) {
+  const audioElement = useRef(null);
+  const [playing, setPlaying] = useState(false);
+  //Dont want songs to play as soon as app starts
+
+  useEffect(() => {
+    if (playing) {
+      audioElement.current.play();
+      //Anytime something changes it will check if audio is playing and will continue playing
+    } else {
+      audioElement.current.pause();
+      // It will just pause the song
+    }
+  });
+
+  const skipSong = (forwards = true) => {};
+
   return (
     <div className="c-player">
-      <audio></audio>
+      <audio ref={audioElement}></audio>
       <h4>Now Playing</h4>
       <PlayerDetails song={props.song} />
       <PlayerControls />
